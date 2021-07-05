800b59d41ed7d405eeafdb4ade754963a036c815
Paul Ashraf <paulashraf14@gmail.com>
2021-07-05 04:44:25 +0200

    strimlit

---
+++ display.py
@@ -0,0 +1,61 @@
+from utils import plot_solution
+from solver import mip_simple
+from generate_data import generate_data, plot_units
+import streamlit as st
+import matplotlib.pyplot as plt
+from functools import partial
+
+def get_units():
+    units, areas_demand = generate_data(sparcity, 
+        radius, 
+        num_units_per_area, 
+        areas, 
+        demand_range, 
+        rent_range, 
+        capacity_kitchen_range, 
+        capacity_restaurant_range,
+        initial_kitchen_range, 
+        initial_restaurant_range
+    )   
+    
+    st.session_state['units'] = units
+    st.session_state['areas_demand'] = areas_demand
+
+    fig = plot_units(units, areas_demand, radius)
+    st.session_state['fig'] = fig
+
+def solve(solver):  
+    if solver == 'mip':
+        solution = mip_simple(st.session_state['units'],  st.session_state['areas_demand'], radius, budget=1000000, cpd=1, r=10)
+        fig = plot_solution(solution,  st.session_state['units'],  st.session_state['areas_demand'])
+        st.session_state['fig'] = fig
+
+if 'units' not in st.session_state:
+    units, areas_demand = generate_data()   
+    st.session_state['units'] = units
+    st.session_state['areas_demand'] = areas_demand
+    st.session_state['fig'] = plt.figure()
+
+
+st.set_page_config(layout="wide")
+st.set_option('deprecation.showPyplotGlobalUse', False)
+left, right = st.beta_columns(2)
+
+with left:
+    st.header('Customise Random Data Ranges')
+
+    sparcity = st.slider('Sparcity', 0  , 100, 15)
+    radius = st.slider('Radius', 0, 100, 10)
+    num_units_per_area = st.slider('Number of units per area range', 0, 20, (2, 3))
+    areas = st.slider('Areas', 0, 10, 6)
+    demand_range = st.slider('Demand Range', 0, int(1e9), (int(1e8), int(1e9)), step=10000)
+    rent_range = st.slider('Rent Range', 0, 20000, (3000, 15000), step=10)
+    capacity_kitchen_range = st.slider('Kitchen Capacity Range', 0, 20000, (2000, 3000), step=10)
+    capacity_restaurant_range = st.slider('Restaurant Capacity Range', 0, 1000, (300, 400), step=10)
+    initial_kitchen_range = st.slider('Kitchen Initial Cost Range', 0, 2000000, (500000, 1000000), step=10000)
+    initial_restaurant_range = st.slider('Restaurant Initial Cost Range', 0, 1000000, (100000, 200000), step=10000)
+
+with right:
+    st.button('Solve w/ MIP', on_click=partial(solve, 'mip'))
+    st.button('Generate Data', on_click=get_units)
+    st.pyplot(st.session_state['fig'], clear_figure=True)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import random
 import math
 import time
+import matplotlib.pyplot as plt
 
 def eucledian_distance(p1, p2):
     return math.dist(p1, p2)
@@ -39,6 +40,54 @@ def get_random_areas(num, sparcity, radius):
         curr_x = 0
 
     return positions
+
+def plot_solution(solution, units, areas_demand):
+     
+    x = [ unit["position"][0] for unit in units ]
+    y = [ unit["position"][1] for unit in units ]
+
+    for i in range(len(solution[2])):
+        for j in range(len(solution[2])):
+            if solution[2][i][j] ==  1:
+                    plt.plot([units[i]["position"][0], units[j]["position"][0]],
+                             [units[i]["position"][1], units[j]["position"][1]], 'r--', alpha=.2)
+
+    for area in areas_demand:
+        plt.gca().add_patch(plt.Circle(area[0], radius=10, alpha=.1))
+        plt.scatter([area[0][0]], [area[0][1]], lw=.4, c='blue', marker='+')
+
+    for i, (x_, y_) in enumerate(zip(x, y)):
+        plt.scatter([x_], [y_], 
+        marker="${}$".format(units[i]["capacity_restaurant"]) if solution[0][i] == 1 else "${}$".format(units[i]["capacity_kitchen"]) if solution[1][i] == 1 else "x",
+        lw=.5, 
+        s=200 if solution[0][i] == 1 or solution[1][i] == 1 else 50,
+        c= "g" if solution[0][i] == 1 else "r" if solution[1][i] == 1 else "black")
+    
+    plt.grid(color='gray', ls = '--', lw = 0.25)
+    plt.gca().set_aspect('equal', adjustable='box')
+    
+    return plt.gcf()
+
+def plot_units(units, areas_demand):
+     
+    x = [ unit["position"][0] for unit in units ]
+    y = [ unit["position"][1] for unit in units ]
+
+    for area in areas_demand:
+        plt.gca().add_patch(plt.Circle(area[0], radius=10, alpha=.1))
+        plt.scatter([area[0][0]], [area[0][1]], lw=.4, c='blue', marker='+')
+
+    for i, (x_, y_) in enumerate(zip(x, y)):
+        plt.scatter([x_], [y_], 
+        marker="x",
+        lw=.5, 
+        s=50,
+        c="black")
+    
+    plt.grid(color='gray', ls = '--', lw = 0.25)
+    plt.gca().set_aspect('equal', adjustable='box')
+    
+    return plt.gcf()
 
 def timed(func):
     def _w(*a, **k):
