7ecfda7df0bd1c25f6bb07e9ab2add3fb8d5f7b0
PeterAbdalla <peter-adel-2007@hotmail.com>
2021-07-15 19:00:15 +0200

    Update generate_data.py

--- generate_data.py
+++ generate_data.py
@@ -1,17 +1,16 @@
 from utils import get_random, get_random_positions, get_random_areas
-import matplotlib.pyplot as plt
 
 
 def generate_data(
         scenario=0,
-        sparcity=10,
-        radius=7,
-        num_units_per_area=(5, 10),
-        areas=3,
-        demand_range=(3000, 10000),
+        sparcity=-10,
+        radius=10,
+        num_units_per_area=(1, 2),
+        areas=6,
+        demand_range=(1000, 1500),
         rent_range=(3000, 15000),
-        capacity_kitchen_range=(1000, 2000),
-        capacity_restaurant_range=(300, 600),
+        capacity_kitchen_range=(1000, 1500),
+        capacity_restaurant_range=(250, 500),
         initial_kitchen_range=(500000, 1000000),
         initial_restaurant_range=(100000, 200000)):
     units = []
@@ -34,26 +33,3 @@ def generate_data(
             })
             j += 1
     return units, areas_demand
-
-
-# def get_correlate(demand, scenario, factor, range):
-#     if scenario == 0:
-#         return get_random(range)
-#     if scenario == 1:
-
-    
-
-def plot_units(units, areas_demand, radius):
-    x = [unit['position'][0] for unit in units]
-    y = [unit['position'][1] for unit in units]
-    for area in areas_demand:
-        plt.gca().add_patch(plt.Circle(area[0], radius, alpha=.1))
-        plt.scatter([area[0][0]], [area[0][1]], lw=.4, c='blue', marker='+')
-    plt.scatter(x, y, marker='x', lw=.5)
-    plt.grid(color='gray', ls='--', lw=0.25)
-    plt.show()
-
-
-if __name__ == '__main__':
-    units, areas_demand = generate_data(areas=12, sparcity=2)
-    plot_units(units, areas_demand, radius=10)
