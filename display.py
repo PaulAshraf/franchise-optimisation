ebee58b7ce710f5e43570a43eded2753e984aff4
PeterAbdalla <peter-adel-2007@hotmail.com>
2021-07-16 02:48:39 +0200

    Update display.py

--- display.py
+++ display.py
@@ -11,9 +11,9 @@ def scenarios(i):
     if i == 0:
         return "No Correlations (Tottaly Random)"
     if i == 1:
-        return "Rent increases with demand"
+        return "Rent increases with area demand"
     if i == 2:
-        return "Capaicty increases with rent + Rent increases with demand"
+        return "Capacity increases with rent price & Rent increases with area demand"
 
 
 def get_units():
@@ -41,24 +41,25 @@ def get_units_from_file():
         string_data = strio.read().splitlines()
         areas = []
         units = []
-        sparcity, radius, num_units_per_area, a, budget, r, u = [int(x) for x in string_data[0].split(" ")]
-        for i in range(1, int(a) + 1):
+        i = 0
+        sparcity, radius, number_of_areas, number_of_units_per_area, \
+        budget, optimization_ratio = [int(x) for x in string_data[i].split(" ")]
+        i += 1
+        while i < number_of_areas + 1:
             x, y, demand = [int(x) for x in string_data[i].split(" ")]
             areas.append([(x, y), demand])
-        for i in range(1 + int(a), 1 + int(a) + int(u)):
-            x, y, j, area, rent, capacity_kitchen, capacity_restaurant, initial_restaurant, initial_kitchen = [int(x)
-                                                                                                               for x in
-                                                                                                               string_data[
-                                                                                                                   i].split(
-                                                                                                                   " ")]
+            i += 1
+        for j in range(number_of_areas * number_of_units_per_area):
+            point_x, point_y, area_number, rent, if_kitchen_capacity, if_restaurant_capacity, \
+            if_restaurant_initial_price, if_kitchen_initial_price = [int(x) for x in string_data[j + i].split(" ")]
             units.append({
-                'position': (x, y),
-                'area': area,
+                'position': (point_x, point_y),
+                'area': area_number - 1,
                 'rent': rent,
-                'capacity_kitchen': capacity_kitchen,
-                'capacity_restaurant': capacity_restaurant,
-                'initial_restaurant': initial_restaurant,
-                'initial_kitchen': initial_kitchen,
+                'capacity_kitchen': if_kitchen_capacity,
+                'capacity_restaurant': if_restaurant_capacity,
+                'initial_restaurant': if_restaurant_initial_price,
+                'initial_kitchen': if_kitchen_initial_price,
                 'initial_index': j
             })
         st.session_state['units'] = units
@@ -73,16 +74,20 @@ def solve(s):
     cpd = 1
     if s == 'Greedy':
         solution = Greedy(units, areas_demand, budget, radius, cpd, r)
-        st.session_state['fig'] = plot_solution(solution, units, areas_demand, radius)
+        st.session_state['fig'], customer, cost = plot_solution(solution, units, areas_demand, radius)
+        print('Greedy', customer, cost)
     elif s == 'Genetics':
         solution = Genetics(units, areas_demand, budget, radius, cpd, r)
+        print('Genetics', solution[0], solution[1])
         st.session_state['fig'] = plot_solution_2(solution[2], units, areas_demand, radius)
     elif s == 'DP':
         solution = DP(units, areas_demand, budget, radius, cpd, r)
+        print('DP', solution[0], solution[1] + solution[2])
         st.session_state['fig'] = plot_solution_2(solution[3], units, areas_demand, radius)
     elif s == 'MIP':
         solution = MIP(units, areas_demand, budget, radius, cpd, r)
-        st.session_state['fig'] = plot_solution(solution, units, areas_demand, radius)
+        st.session_state['fig'], customer, cost = plot_solution(solution, units, areas_demand, radius)
+        print('MIP', customer, cost)
 
 
 if 'units' not in st.session_state:
@@ -102,11 +107,18 @@ with left:
 
         The input file should be using the following format:
 
-        First line: sparcity, radius, num\_units\_per\_area, areas(a), budget, r, units(n)
+        _First line_:
         
-        _For n lines_: x, y, j, area, rent, capacity\_kitchen, capacity\_restaurant, initial\_restaurant, initial\_kitchen
+            sparcity, radius, number_of_areas, number_of_units_per_area, budget, optimization_ratio
         
-        _For a lines_: x, y, demand
+        _For number_of_areas lines_:
+        
+            center_x, center_y, demand_per_area
+        
+        _For (number_of_units_per_area*number_of_areas) lines_:
+        
+            point_x, point_y, area_number, rent, if_kitchen_capacity, if_restaurant_capacity, if_restaurant_initial_price,
+            if_kitchen_initial_price
 
     ''')
     file = st.file_uploader('Upload File')
@@ -114,22 +126,24 @@ with left:
     st.header('Random Data')
     sparcity = st.slider('Sparcity', -20, 20, -10)
     radius = st.slider('Radius', 0, 20, 10)
-    num_units_per_area = st.slider('Number of units per area range', 1, 6, (2, 3))
-    areas = st.slider('Areas', 0, 8, 6)
-    demand_range = st.slider('Demand Range', 0, 2000, (1000, 1500), step=50)
-    rent_range = st.slider('Rent Range', 0, 20000, (3000, 15000), step=100)
-    capacity_kitchen_range = st.slider('Kitchen Capacity Range', 0, 5000, (1000, 1500), step=100)
-    capacity_restaurant_range = st.slider('Restaurant Capacity Range', 0, 1000, (250, 500), step=50)
-    initial_kitchen_range = st.slider('Kitchen Initial Cost Range', 0, 2000000, (500000, 1000000), step=10000)
-    initial_restaurant_range = st.slider('Restaurant Initial Cost Range', 0, 1000000, (100000, 200000), step=10000)
-    budget = st.slider('Budget', int(4e6), int(1e9), int(4e6), step=int(1e6), format="%e")
-    r = st.slider('Optimisation Ratio', int(1e3), int(1e9), int(1e3), step=int(1e6), format="%e")
+    areas = st.slider('Number of areas', 0, 8, 6)
+    num_units_per_area = st.slider('Number of units per area', 1, 6, (2, 3))
+    demand_range = st.slider('Demand range per area', 0, 2000, (1000, 1500), step=50)
+    rent_range = st.slider('Rent range per area', 0, 20000, (3000, 15000), step=100)
+    capacity_kitchen_range = st.slider('Kitchen capacity range for all kitchens', 0, 5000, (1000, 1500), step=100)
+    capacity_restaurant_range = st.slider('Restaurant capacity range for all restaurants', 0, 1000, (250, 500), step=50)
+    initial_kitchen_range = st.slider('Kitchen initial cost range for all kitchens', 0, 2000000, (500000, 1000000),
+                                      step=10000)
+    initial_restaurant_range = st.slider('Restaurant initial cost range for all restaurants', 0, 1000000,
+                                         (100000, 200000), step=10000)
+    budget = st.slider('Total budget', int(4e6), int(1e9), int(4e6), step=int(1e6), format="%e")
+    r = st.slider('Optimization ratio', int(1e6), int(1e9), int(1e6), step=int(1e6), format="%e")
     scenario = st.radio('Scenario', range(3), format_func=scenarios)
     st.button('Generate Random Data', on_click=get_units)
 
 with right:
+    st.pyplot(st.session_state['fig'], clear_figure=True)
     st.button('Greedy', on_click=partial(solve, 'Greedy'))
     st.button('Genetics', on_click=partial(solve, 'Genetics'))
     st.button('DP', on_click=partial(solve, 'DP'))
     st.button('MIP', on_click=partial(solve, 'MIP'))
-    st.pyplot(st.session_state['fig'], clear_figure=True)
