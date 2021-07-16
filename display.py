5013e82f74fd209e5a3a38979ad0fbcf1b60483f
PeterAbdalla <peter-adel-2007@hotmail.com>
2021-07-16 06:47:30 +0200

    Update display.py

--- display.py
+++ display.py
@@ -1,3 +1,5 @@
+import base64
+import time
 from io import StringIO
 from utils import plot_solution, plot_solution_2, plot_units
 from solver import Greedy, Genetics, DP, MIP
@@ -5,20 +7,11 @@ from generate_data import generate_data
 import streamlit as st
 from functools import partial
 import matplotlib.pyplot as plt
-
-
-def scenarios(i):
-    if i == 0:
-        return "No Correlations (Tottaly Random)"
-    if i == 1:
-        return "Rent increases with area demand"
-    if i == 2:
-        return "Capacity increases with rent price & Rent increases with area demand"
+import pandas as pd
 
 
 def get_units():
-    units, areas_demand = generate_data(scenario,
-                                        sparcity,
+    units, areas_demand = generate_data(sparcity,
                                         radius,
                                         num_units_per_area,
                                         areas,
@@ -33,21 +26,24 @@ def get_units():
     st.session_state['areas_demand'] = areas_demand
     fig = plot_units(units, areas_demand, radius)
     st.session_state['fig'] = fig
+    st.session_state['output'] = [{}, {}, {}, {}]
 
 
-def get_units_from_file():
-    if file is not None:
+def summary():
+    results = []
+    f = 0
+    for file in multiple_files:
         strio = StringIO(file.getvalue().decode("utf-8"), newline='\n')
         string_data = strio.read().splitlines()
-        areas = []
+        areas_demand = []
         units = []
         i = 0
         sparcity, radius, number_of_areas, number_of_units_per_area, \
-        budget, optimization_ratio = [int(x) for x in string_data[i].split(" ")]
+        budget, r = [int(x) for x in string_data[i].split(" ")]
         i += 1
         while i < number_of_areas + 1:
             x, y, demand = [int(x) for x in string_data[i].split(" ")]
-            areas.append([(x, y), demand])
+            areas_demand.append([(x, y), demand])
             i += 1
         for j in range(number_of_areas * number_of_units_per_area):
             point_x, point_y, area_number, rent, if_kitchen_capacity, if_restaurant_capacity, \
@@ -62,10 +58,43 @@ def get_units_from_file():
                 'initial_kitchen': if_kitchen_initial_price,
                 'initial_index': j
             })
-        st.session_state['units'] = units
-        st.session_state['areas_demand'] = areas
-        fig = plot_units(units, areas, radius)
-        st.session_state['fig'] = fig
+        # Greedy
+        start = time.time()
+        algo1_sol = Greedy(units, areas_demand, budget, radius, 1, r)
+        algo1_t = time.time() - start
+        _, algo1_z, algo1_c = plot_solution(algo1_sol, units, areas_demand, radius)
+        # Genetics
+        start = time.time()
+        algo2_c, algo2_z, _ = Genetics(units, areas_demand, budget, radius, 1, r)
+        algo2_t = time.time() - start
+        # DP
+        start = time.time()
+        algo3_c, algo3_z1, algo3_z2, _ = DP(units, areas_demand, budget, radius, 1, r)
+        algo3_z = algo3_z1 + algo3_z2
+        algo3_t = time.time() - start
+        # MIP
+        start = time.time()
+        algo4_sol = MIP(units, areas_demand, budget, radius, 1, r)
+        algo4_t = time.time() - start
+        _, algo4_z, algo4_c = plot_solution(algo4_sol, units, areas_demand, radius)
+        # result
+        results.append([f, len(units), budget,
+                        algo1_c, algo1_z, algo1_t,
+                        algo2_c, algo2_z, algo2_t,
+                        algo3_c, algo3_z, algo3_t,
+                        algo4_c, algo4_z, algo4_t])
+        f += 1
+    columns = ['test_case', 'number_of_units', 'budget',
+               'algo1_c', 'algo1_z', 'algo1_t',
+               'algo2_c', 'algo2_z', 'algo2_t',
+               'algo3_c', 'algo3_z', 'algo3_t',
+               'algo4_c', 'algo4_z', 'algo4_t']
+    df_results = pd.DataFrame(results, columns=columns)
+    csv = df_results.to_csv(index=False).encode()
+    b64 = base64.b64encode(csv).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="download.csv">Download summary file</a>'
+    return href
+
 
 
 def solve(s):
@@ -73,21 +102,27 @@ def solve(s):
     areas_demand = st.session_state['areas_demand']
     cpd = 1
     if s == 'Greedy':
+        t = time.time()
         solution = Greedy(units, areas_demand, budget, radius, cpd, r)
         st.session_state['fig'], customer, cost = plot_solution(solution, units, areas_demand, radius)
-        print('Greedy', customer, cost)
+        st.session_state['output'][0] = {'Algo': 'Greedy', 'customers': customer, 'cost': cost, 'time': time.time() - t}
     elif s == 'Genetics':
+        t = time.time()
         solution = Genetics(units, areas_demand, budget, radius, cpd, r)
-        print('Genetics', solution[0], solution[1])
         st.session_state['fig'] = plot_solution_2(solution[2], units, areas_demand, radius)
+        st.session_state['output'][1] = {'Algo': 'Genetics', 'customers': solution[0], 'cost': solution[1],
+                                         'time': time.time() - t}
     elif s == 'DP':
+        t = time.time()
         solution = DP(units, areas_demand, budget, radius, cpd, r)
-        print('DP', solution[0], solution[1] + solution[2])
         st.session_state['fig'] = plot_solution_2(solution[3], units, areas_demand, radius)
+        st.session_state['output'][2] = {'Algo': 'DP', 'customers': solution[0], 'cost': solution[1] + solution[2],
+                                         'time': time.time() - t}
     elif s == 'MIP':
+        t = time.time()
         solution = MIP(units, areas_demand, budget, radius, cpd, r)
         st.session_state['fig'], customer, cost = plot_solution(solution, units, areas_demand, radius)
-        print('MIP', customer, cost)
+        st.session_state['output'][3] = {'Algo': 'MIP', 'customers': customer, 'cost': cost, 'time': time.time() - t}
 
 
 if 'units' not in st.session_state:
@@ -95,6 +130,8 @@ if 'units' not in st.session_state:
     st.session_state['units'] = units
     st.session_state['areas_demand'] = areas_demand
     st.session_state['fig'] = plt.figure()
+if 'output' not in st.session_state:
+    st.session_state['output'] = [{}, {}, {}, {}]
 
 st.set_page_config(layout='wide')
 st.set_option('deprecation.showPyplotGlobalUse', False)
@@ -121,8 +158,8 @@ with left:
             if_kitchen_initial_price
 
     ''')
-    file = st.file_uploader('Upload File')
-    st.button('Generate Data from File', on_click=get_units_from_file)
+    multiple_files = st.file_uploader("Multiple File Uploader", accept_multiple_files=True)
+    st.markdown(summary(), unsafe_allow_html=True)
     st.header('Random Data')
     sparcity = st.slider('Sparcity', -20, 20, -10)
     radius = st.slider('Radius', 0, 20, 10)
@@ -136,9 +173,8 @@ with left:
                                       step=10000)
     initial_restaurant_range = st.slider('Restaurant initial cost range for all restaurants', 0, 1000000,
                                          (100000, 200000), step=10000)
-    budget = st.slider('Total budget', int(4e6), int(1e9), int(4e6), step=int(1e6), format="%e")
+    budget = st.slider('Total budget', int(1e6), int(1e8), int(1e6), step=int(1e6), format="%e")
     r = st.slider('Optimization ratio', int(1e6), int(1e9), int(1e6), step=int(1e6), format="%e")
-    scenario = st.radio('Scenario', range(3), format_func=scenarios)
     st.button('Generate Random Data', on_click=get_units)
 
 with right:
@@ -147,3 +183,11 @@ with right:
     st.button('Genetics', on_click=partial(solve, 'Genetics'))
     st.button('DP', on_click=partial(solve, 'DP'))
     st.button('MIP', on_click=partial(solve, 'MIP'))
+    st.header("Areas & Demands")
+    df_areas_demand = pd.DataFrame.from_dict(st.session_state['areas_demand'])
+    df_areas_demand.columns = ['center', 'area_demand']
+    st.dataframe(df_areas_demand)
+    st.header("Units Info.")
+    st.dataframe(pd.DataFrame.from_dict(st.session_state['units']))
+    st.header("Results")
+    st.dataframe(pd.DataFrame.from_dict(st.session_state['output']))
